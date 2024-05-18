a9803acf2a3b687286aad21ded788e367c458753
PDK13 <khoa199960@gmail.com>
2024-05-18 14:05:57 +0700

    update x2 x4

--- cocos-3.6.3-playable-meno/assets/Script/base/item/ItemX2.ts
+++ cocos-3.6.3-playable-meno/assets/Script/base/item/ItemX2.ts
@@ -1,30 +1,27 @@
 import { _decorator, Collider2D, Component, Contact2DType, director, IPhysics2DContact, Node, RigidBody2D } from 'cc';
-import GameEvent from '../GameEvent';
+import GameEvent from '../../GameEvent';
+import { PlayerController } from '../../player/PlayerController';
 const { ccclass, property } = _decorator;
 
-@ccclass('ObjectX4')
-export class ObjectX4 extends Component {
-    
+@ccclass('ItemX2')
+export class ItemX2 extends Component {
+
     m_stop: boolean = false;
 
     onLoad() {
         let collider = this.getComponent(Collider2D);
-        if (collider) {
+        if (collider)
             collider.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
-            //collider.on(Contact2DType.END_CONTACT, this.onEndContact, this);
-        }
     }
 
-    onBeginContact (selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null)
-    {      
-        if(this.m_stop)
-            return;         
-        switch(otherCollider.tag)
-        {
+    onBeginContact(selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null) {
+        if (this.m_stop)
+            return;
+        switch (otherCollider.tag) {
             case 100://player     
-                this.m_stop = true;  
+                this.m_stop = true;
                 setTimeout(() => {
-                    director.emit(GameEvent.PLAYER_X4, true);    
+                    otherCollider.getComponent(PlayerController).onX2(true);
                     this.getComponent(RigidBody2D).sleep();
                     this.node.destroy();
                 }, 1);
--- cocos-3.6.3-playable-meno/assets/Script/player/PlayerController.ts
+++ cocos-3.6.3-playable-meno/assets/Script/player/PlayerController.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Vec2, RigidBody2D, Vec3, tween, sp } from 'cc';
+import { _decorator, Component, Vec2, RigidBody2D, Vec3, tween, sp, Collider2D } from 'cc';
 import { BaseSpine } from '../base/BaseSpine';
 const { ccclass, property } = _decorator;
 
@@ -26,5 +26,48 @@ export class PlayerController extends Component {
 
     get isBig() {
         return this.m_big;
+    }
+
+    //
+
+    m_x2: boolean = false;
+    m_x4: boolean = false;
+
+    onX2(Active: boolean) {
+        this.m_x2 = Active;
+        //
+        if (this.m_x4)
+            return;
+        //
+        let BaseScale: Vec3 = Vec3.ONE;
+        let Ratio = Active ? 1.75 : 1;
+        let Colliders = this.getComponents(Collider2D);
+        setTimeout(() => {
+            tween(this.node).to(0.25, { scale: BaseScale.clone().multiplyScalar(Ratio) }).call(() => {
+                Colliders.forEach(c => {
+                    c.apply();
+                });
+            }).start();
+        }, 1);
+    }
+
+    onX4(Active: boolean) {
+        this.m_x4 = Active;
+        //
+        if (!Active && this.m_x2) {
+            this.onX2(true);
+            return;
+        }
+        //
+        let BaseScale: Vec3 = Vec3.ONE;
+        let Ratio = Active ? 4 : 1;
+        let Colliders = this.getComponents(Collider2D);
+        setTimeout(() => {
+            tween(this.node).to(0.25, { scale: BaseScale.clone().multiplyScalar(Ratio) }).call(() => {
+                Colliders.forEach(c => {
+                    c.apply();
+                });
+            }).start();
+        }, 1);
     }
 }
